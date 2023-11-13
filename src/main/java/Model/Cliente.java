089a29800847877af57ce049fafae91984b1c750
alaraco <144190360+alaraco@users.noreply.github.com>
2023-11-12 21:10:43 -0500

    Add files via upload

---
+++ src/main/java/Model/Cliente.java
@@ -0,0 +1,51 @@
+package Model;
+
+public class Cliente {
+	
+	private int id;
+	private String dni;
+	private String nombre;
+	private String correo;
+	private int telefono;
+	private boolean estado;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	
+	
+}
