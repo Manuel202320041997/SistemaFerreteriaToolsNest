1846be84dd5a81fb99e91ff2c0c2b4387387421e
alaraco <144190360+alaraco@users.noreply.github.com>
2023-11-13 15:12:35 -0500

    Add files via upload

--- src/main/java/Dao/InventarioDao.java
+++ src/main/java/Dao/InventarioDao.java
@@ -6,11 +6,5 @@ import Model.Inventario;
 public interface InventarioDao {
     List<Inventario> listarInventario();
 
-    void agregarProducto(Inventario producto);
-
-    void eliminarProducto(int idProducto);
-
-    void editarProducto(int idProducto);
-
     Inventario buscarProductoPorId(int idProducto);
 }
