f20f7176fbd71de8c0f9b4b3a345d9934aa0be58
Vemto神 <143888468+Manuel202320041997@users.noreply.github.com>
2023-11-14 13:50:39 -0500

    Add files via upload
    Implementación de Archivos Dao de Usuario, Conexion, y Rol

--- src/main/java/Dao/UsuarioDao.java
+++ src/main/java/Dao/UsuarioDao.java
@@ -10,7 +10,9 @@ public interface UsuarioDao {
 	
     public void eliminarUsuario(int idUsuario);
     
-    public void editarUsuario(int idUsuario);
+    public void editarUsuario(Usuario usuario);
     
-    public Usuario buscarUsuarioPorId(int idUsuario);
+    public Usuario buscarUsuarioPorDni(int dniUsuario);
+    
+    public boolean verificarSiExisteDni(int dniUsuario);
 }
