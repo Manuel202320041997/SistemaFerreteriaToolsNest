52c6b75a86e3002cc5f61e2a7e320dd42f312b84
alaraco <144190360+alaraco@users.noreply.github.com>
2023-11-17 21:32:50 -0500

    Add files via upload

--- src/main/java/Controller/ClienteController.java
+++ src/main/java/Controller/ClienteController.java
@@ -11,9 +11,12 @@ import Dao.ClienteDao;
 public class ClienteController {
 	
 	private ClienteDaoImpl clienteDaoImpl = null;
+	
+
     
     public ClienteController(){
         clienteDaoImpl = new ClienteDaoImpl();
+        
     }
 
     public List<Cliente> listarCliente(){
@@ -38,14 +41,38 @@ public class ClienteController {
    	    }
     }
     
-    public void eliminarCliente(int idCliente) {
-    	clienteDaoImpl.eliminarCliente(idCliente);
+    public void editarCliente(Cliente cliente) {
+        try {
+            // Suponiendo que tienes un método en tu Dao para buscar cliente por ID
+            Cliente clienteExistente = clienteDaoImpl.buscarClientePorId(cliente.getId());
+
+            if (clienteExistente != null) {
+                // Actualizar los campos del cliente existente
+                clienteExistente.setDni(cliente.getDni());
+                clienteExistente.setNombre(cliente.getNombre());
+                clienteExistente.setCorreo(cliente.getCorreo());
+                clienteExistente.setTelefono(cliente.getTelefono());
+
+                // Luego, guarda los cambios en la base de datos
+                clienteDaoImpl.editarCliente(clienteExistente);
+            } else {
+                // Manejar el caso donde el cliente no existe
+                throw new IllegalArgumentException("Cliente no encontrado para editar");
+            }
+        } catch (Exception ex) {
+            // Manejar excepciones específicas, si es necesario
+            throw new RuntimeException("Error al editar el cliente", ex);
+        }
     }
-    
+  
     public Cliente buscarClientePorId(int idCliente) {
         return clienteDaoImpl.buscarClientePorId(idCliente);
     }
 
+    public boolean verificarSiExisteDni(int dniUsuario) {
+		return clienteDaoImpl.verificarSiExisteDni(dniUsuario);
+	}
+    
     public int obtenerIdClientePorNombre(String nombreCliente){
 		return clienteDaoImpl.obtenerIdClientePorNombre(nombreCliente);
    }
