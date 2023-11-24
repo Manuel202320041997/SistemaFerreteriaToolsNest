39792d11899b23cbc4ee5dabd279b79c80883461
alaraco <144190360+alaraco@users.noreply.github.com>
2023-11-24 12:24:40 -0500

    Add files via upload

---
+++ src/main/java/Controller/ReporteController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import DaoImpl.ReporteDaoImpl;
+import Model.DetalleVenta;
+import Model.Reporte;
+
+
+public class ReporteController {
+ArrayList<Reporte> listarReporte = new ArrayList<>();
+private ReporteDaoImpl reporteDaoImpl = null;
+
+    
+    public ReporteController(){
+        reporteDaoImpl = new ReporteDaoImpl();
+    }
+    
+    public List<Reporte> listarReporte(){
+    	List<Reporte> listarReporte = null;
+    	listarReporte = reporteDaoImpl.listarReporte();
+    	return listarReporte;
+    }
+    
+	public void ReportesClientes() {
+		
+}
+}
+	
